ca285217dc075439b106132c97812c712519b781
hclee <heechul4296@gmail.com>
2022-10-15 19:01:00 +0900

    spring security remember me source

---
+++ src/main/java/study/security/rememberme/RememberMeApplication.java
@@ -0,0 +1,13 @@
+package study.security.rememberme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberMeApplication.class, args);
+    }
+
+}
